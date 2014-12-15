c64dbe1315fb43c66a2650c0bf044bd85dfc887c
PFei_He <498130877@qq.com>
2014-12-15 15:38:16 +0800

    回滚到0.1.0

--- Demo/Demo/controllers/RootVC.m
+++ Demo/Demo/controllers/RootVC.m
@@ -41,33 +41,12 @@
 
 - (void)loadTabView
 {
-    if (!self.tabView) self.tabView = [[PFTabView alloc] initWithFrame:CGRectMake(0, 0, 320, self.view.frame.size.height - 64) delegate:nil];
+    self.tabView = [[PFTabView alloc] initWithFrame:CGRectMake(0, 0, 320, self.view.frame.size.height - 64) delegate:nil];
+    self.tabView.heightOfItem = 35;
 
-    [self.tabView numberOfItemUsingBlock:^NSInteger(PFTabView *tabView) {
-        return self.viewControllers.count;
-    }];
-    [self.tabView setupViewControllerUsingBlock:^UIViewController *(PFTabView *tabView, NSInteger index) {
-        return self.viewControllers[index];
-    }];
-    [self.tabView textSizeOfItemUsingBlock:^CGSize(PFTabView *tabView) {
-        return CGSizeMake((self.view.frame.size.width) / 4, 30.0f);
-    }];
-    [self.tabView didSelectItemUsingBlock:^(PFTabView *tabView, NSInteger index) {
-        if (index == 0) {
-            NSLog(@"首页");
-        } else if (index == 1) {
-            NSLog(@"新闻");
-        } else if (index == 2) {
-            NSLog(@"热点");
-        } else if (index == 3) {
-            NSLog(@"回复");
-        }
-    }];
-    [self.view addSubview:self.tabView];
-}
+    self.tabView.itemNormalColor = [UIColor blackColor];
+    self.tabView.itemSelectedColor = [UIColor cyanColor];
 
-- (NSArray *)viewControllers
-{
     self.home = [[BaseVC alloc] init];
     self.home.title = @"首页";
     self.home.view.backgroundColor = [UIColor cyanColor];
@@ -88,7 +67,46 @@
     self.reply.view.backgroundColor = [UIColor greenColor];
     self.reply.type = 3;
 
-    return @[self.home, self.news, self.hotspot, self.reply];
+    [self.tabView numberOfItemInTabViewUsingBlock:^NSUInteger(PFTabView *tabView) {
+        return 4;
+    }];
+    [self.tabView viewControllerOfItemAtIndexUsingBlock:^UIViewController *(PFTabView *tabView, NSUInteger index) {
+        if (index == 0) {
+            return self.home;
+        } else if (index == 1) {
+            return self.news;
+        } else if (index == 2) {
+            return self.hotspot;
+        } else if (index == 3) {
+            return self.reply;
+        } else {
+            return nil;
+        }
+    }];
+    [self.tabView textSizeOfItemInTabViewUsingBlock:^CGSize(PFTabView *tabView) {
+        return CGSizeMake((self.view.frame.size.width - 40) / 4, 30.0f);
+    }];
+    [self.tabView didSelectItemAtIndexUsingBlock:^(PFTabView *tabView, NSUInteger index) {
+        if (index == 0) {
+            BaseVC *home = nil;
+            home = self.home;
+            NSLog(@"首页");
+        } else if (index == 1) {
+            BaseVC *news = nil;
+            news = self.news;
+            NSLog(@"新闻");
+        } else if (index == 2) {
+            BaseVC *hotspot = nil;
+            hotspot = self.hotspot;
+            NSLog(@"热点");
+        } else if (index == 3) {
+            BaseVC *reply = nil;
+            reply = self.reply;
+            NSLog(@"回复");
+        }
+    }];
+    [self.tabView loadSubviews];
+    [self.view addSubview:self.tabView];
 }
 
 #pragma mark - Memory Management
